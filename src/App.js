454b36093ffd80739149fe01956d53bc89641262
maria fernanda duarte <mfda.1396@gmail.com>
2026-03-19 21:10:44 +0100

    force deploy

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+console.log("VERSION NUEVA");
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 // import Login from "./pages/Login"; ← opcional quitar
