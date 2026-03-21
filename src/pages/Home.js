cdbedf644ee8a3f662b07cdccfe099195904a04b
maria fernanda duarte <mfda.1396@gmail.com>
2026-03-22 00:30:04 +0100

    fix build error remove useNavigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import { createService } from "../services/api";
 import Navbar from "../components/Navbar";
