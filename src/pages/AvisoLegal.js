498bbb5bc2467fdd80b6e38f92ecb6608d3a25e5
maria fernanda duarte <mfda.1396@gmail.com>
2026-04-11 19:41:07 +0200

    pages legal-privacity-cookies

--- src/pages/AvisoLegal.js
+++ src/pages/AvisoLegal.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "./Legal.css";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import "./Legal.css";
 
 function AvisoLegal() {
     return (
