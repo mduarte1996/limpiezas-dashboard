910448ac60061a25abc3e96ded4d12cea9afab08
maria fernanda duarte <mfda.1396@gmail.com>
2026-04-15 16:32:06 +0200

    pagina limpieza pisos turisticos

--- src/pages/PisosTuristicos.js
+++ src/pages/PisosTuristicos.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import "./legal.css";
+import "../styles/legal.css";
 
 function PisosTuristicos() {
   return (
