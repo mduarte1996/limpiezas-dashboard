00fadc4cccc8a8caebfcec44b83816a0b0b561f3
maria fernanda duarte <mfda.1396@gmail.com>
2026-07-27 22:59:09 +0200

    inicio traduccion i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,36 +1,35 @@
 import i18n from "i18next";
+
 import { initReactI18next } from "react-i18next";
 
-const resources = {
-  es: {
-    translation: {
-      inicio: "Inicio",
-      servicios: "Servicios",
-      sobre: "Sobre nosotros",
-      contacto: "Contacto",
-      solicitar: "Solicitar presupuesto"
-    }
-  },
-  en: {
-    translation: {
-      inicio: "Home",
-      servicios: "Services",
-      sobre: "About us",
-      contacto: "Contact",
-      solicitar: "Request quote"
-    }
-  }
-};
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import es from "./locales/es/translation.json";
+import en from "./locales/en/translation.json";
 
 i18n
+.use(LanguageDetector)
 .use(initReactI18next)
 .init({
-  resources,
-  lng: "es",
-  fallbackLng: "es",
-  interpolation: {
-    escapeValue: false
-  }
+
+    fallbackLng: "es",
+
+    interpolation: {
+        escapeValue: false
+    },
+
+    resources: {
+
+        es: {
+            translation: es
+        },
+
+        en: {
+            translation: en
+        }
+
+    }
+
 });
 
 export default i18n;
